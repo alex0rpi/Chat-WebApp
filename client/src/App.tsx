61c508911124757e0e095d8854d6f3a6236271d0
alex0rpi <orpi.alex@hotmail.com>
2023-04-27 16:48:52 +0200

    initial context setup no errors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,17 @@
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import RoomChat from './pages/RoomChat';
 import WelcomeChat from './pages/WelcomeChat';
-import { ISocketContext, initialSocketContext, SocketContext } from './context/SocketContext';
-import { Socket } from 'socket.io-client';
+import { initialSocketContext, SocketContext, reducerFunction } from './context/SocketContext';
 import { useReducer } from 'react';
 
 function App() {
-  // Everything is an 'update' action, which either creates, updates or deletes rooms/messages/users
-
-  interface IReducerActions {
-    type: 'update_socket' | 'update_uid' | 'update_users' | 'update_messages' | 'update_rooms';
-    payload: string | string[] | Socket; // only 3 things can pass to the reducer
-  }
-  const reducerFunction = (state: ISocketContext, action: IReducerActions) => {
-    console.log('Hola');
-    if(action.type === 'update_socket') {
-      return { ...state, socket: action.payload as Socket };
-    }
-  };
-
   const [appState, dispatch] = useReducer(reducerFunction, initialSocketContext);
 
   return (
     <div className="App">
       {/* Apply the Provider property of the Socket Context to the app and
       feed it with the context variables from the reducer defined */}
-      <SocketContext.Provider value={{ appState, dispatch }}>
+      <SocketContext.Provider value={{ appState: appState, appDispatch: dispatch }}>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Navigate to="/welcome" />} />
--- client/src/context/SocketContext.ts
+++ client/src/context/SocketContext.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 // create a context for authentication
-import { createContext } from 'react';
+import { Dispatch, createContext } from 'react';
 import { Socket } from 'socket.io-client';
 
 export interface ISocketContext {
@@ -9,6 +9,35 @@ export interface ISocketContext {
   users: string[]; // list of all the ids already connected
   messages: string[]; // list of all the messages (in a room or in the welcome chat)
   rooms: string[]; // list of all the rooms
+}
+
+interface IReducerActions {
+  type: 'update_socket' | 'update_uid' | 'update_users' | 'update_messages' | 'remove_user' | 'update_rooms';
+  payload: string | string[] | Socket; // only 3 things can pass to the reducer
+}
+export const reducerFunction = (state: ISocketContext, action: IReducerActions) => {
+  if (action.type === 'update_socket') {
+    return { ...state, socket: action.payload as Socket };
+  }
+  if (action.type === 'update_uid') {
+    return { ...state, uid: action.payload as string };
+  }
+  if (action.type === 'update_users') {
+    return { ...state, users: action.payload as string[] };
+  }
+  if (action.type === 'remove_user') {
+    return { ...state, users: state.users.filter((uid) => uid !== action.payload) };
+  }
+  return state;
+};
+
+// ### NOW WE CREATE THE CONTEXT ### //
+// up until now, we have only created the types and the reducer function that will be used by the context for managing the state
+
+export interface ISocketContextProps {
+  // this is the type of the value prop of the context provider
+  appState: ISocketContext;
+  appDispatch: React.Dispatch<IReducerActions>; // it will look for that reducer function
 }
 
 export const initialSocketContext: ISocketContext = {
@@ -20,4 +49,7 @@ export const initialSocketContext: ISocketContext = {
 }; // this is the default state of the context
 // I don't put functions on the state, I rather use a reducer to update the state.
 
-export const SocketContext = createContext(initialSocketContext);
+export const SocketContext = createContext<ISocketContextProps>({
+  appState: initialSocketContext,
+  appDispatch: () => {},
+});
