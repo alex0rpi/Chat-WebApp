20cddd5bb85b546bf7116d99b3a4ab3930205f1e
alex0rpi <orpi.alex@hotmail.com>
2023-05-15 19:20:08 +0200

    added async on a callback on socket.emit integrate

--- client/src/context/SocketCtxWrapper.tsx
+++ client/src/context/SocketCtxWrapper.tsx
@@ -1,6 +1,10 @@
 import React, { PropsWithChildren, useEffect, useReducer, useState } from 'react';
 import { User } from '../Interfaces/Interfaces';
-import { initialSocketContext, SocketContextProvider, reducerFunction } from './SocketContext';
+import {
+  initialSocketContext,
+  SocketContextProvider,
+  reducerFunction,
+} from './SocketContext';
 import { useSocket } from '../hooks/useSocketHook';
 
 export interface ISocketContextComponentProps extends PropsWithChildren {
@@ -61,28 +65,34 @@ const SocketCtxWrapper: React.FunctionComponent<ISocketContextComponentProps> = 
   const SendIntegrate = async (): Promise<void> => {
     console.log('Sending Integrate to server ...');
 
-    socket.emit('integrate', loggedUser, (current_uid: string, logged_users: string[]) => {
-      // console.info('User integrate callback message received')
-      // This function is called when the server responds with the current user id and the list of connected users
-      dispatch({ type: 'update_current_uid', payload: current_uid });
-      dispatch({ type: 'update_logged_users', payload: logged_users });
+    socket.emit(
+      'integrate',
+      loggedUser,
+      async (current_uid: string, logged_users: string[]) => {
+        // console.info('User integrate callback message received')
+        // This function is called when the server responds with the current user id and the list of connected users
+        dispatch({ type: 'update_current_uid', payload: current_uid });
+        dispatch({ type: 'update_logged_users', payload: logged_users });
 
-      setLoading(false);
-    });
+        setLoading(false);
+      }
+    );
   };
 
   useEffect(() => {
     socket.connect();
     dispatch({ type: 'update_socket', payload: socket });
     StartListeners();
-    SendIntegrate(); // precedir amb un void per indicar que no ens interessa el què retorni.
+    void SendIntegrate(); // precedir amb un void per indicar que no ens interessa el què retorni.
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   if (loading) return <p>Loading Socket IO...</p>;
 
   return (
-    <SocketContextProvider value={{ appState, dispatch }}>{children}</SocketContextProvider>
+    <SocketContextProvider value={{ appState, dispatch }}>
+      {children}
+    </SocketContextProvider>
   );
 };
 
