0d65d8887fcb7689f7861fed704c28007ce70adf
alex0rpi <orpi.alex@hotmail.com>
2023-05-24 21:37:39 +0200

    inverted group name is checked if it already exists

--- server/src/models/evListeners-Emitters/createRoom.ts
+++ server/src/models/evListeners-Emitters/createRoom.ts
@@ -7,7 +7,13 @@ export const createRoom = async (serverSocket: ServerSocket, data: RoomData) => 
   try {
     // check if roomName already is in use
     const existingRoom = await roomRepository!.retrieveRoomByName(roomName);
-    if (!existingRoom) {
+    // In case it is a private room, check if it exists with the names inverted.
+    const invertedRoomName = '🔏' + roomName.slice(2).split('↔').reverse().join('↔');
+    const existingRoomInverted = await roomRepository!.retrieveRoomByName(
+      invertedRoomName
+    );
+    // console.log(roomName, invertedRoomName);
+    if (!existingRoom && !existingRoomInverted) {
       await roomRepository!.createRoom(roomName, isPrivate);
       // Get the list all rooms with their users
       const rooms = await roomRepository!.getAllRoomsAndUsers();
